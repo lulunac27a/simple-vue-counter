2bda09711c76b6a27577942bd3173d920773f462
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-09-17 18:56:45 -0500

    Initialize simple counter

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+const reset = () => {
+  count.value = 0;
+};
+</script>
+
+<template>
+  <div class="counter">
+    <h2>Simple Counter</h2>
+    <p class="count">Count: <span>{{ count }}</span></p>
+    <div class="button">
+      <button @click="increment">Increase Count</button>
+      <button @click="reset">Reset Count</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
